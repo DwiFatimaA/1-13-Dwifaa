cd225e3e3ccfdf94f6be5372a029aabdffb57518
DwiFatimaA <3103120072@student.smktelkom-pwt.sch.id>
2022-02-07 08:32:31 +0700

    modul 3

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -38,4 +39,6 @@ Route::get('/gallery', function () {
         "title" => "Gallery"
     ]);
 });
+
+Route::resource('/contacts', ContactController::class);
 
