20494d900484a41ceb6ccaa70f3e41b971f586db
DwiFatimaA <3103120072@student.smktelkom-pwt.sch.id>
2022-02-24 15:33:43 +0700

    modul 5

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,8 @@ use Illuminate\Support\Facades\Route;
 //});
 
 Route::get('/', function () {
-    return view('home', [
-        "title" => "Home"
+    return view('index', [
+        "title" => "Beranda"
     ]);
 });
 
@@ -41,4 +41,12 @@ Route::get('/gallery', function () {
 });
 
 Route::resource('/contacts', ContactController::class);
+
+
+
+Auth::routes();
+
+Route::group(['middleware' => ['auth']], function (){
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+});
 
